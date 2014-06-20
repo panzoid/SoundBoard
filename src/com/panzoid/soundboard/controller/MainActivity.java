ba9ea40fa60f361dd1a8a65697f4accf122c794e
Christopher Guevara <chrismguevara@gmail.com>
2014-06-19 19:55:52 -0700

    Trying out SoundPool
    There seems to be less delay between pressing the button and hearing the
    sound. There's still a noticeable delay when stopping and then replaying
    the sound.

--- src/com/panzoid/soundboard/controller/MainActivity.java
+++ src/com/panzoid/soundboard/controller/MainActivity.java
@@ -6,6 +6,8 @@ import com.panzoid.soundboard.model.event.Event;
 import com.panzoid.soundboard.model.state.StateMachine;
 
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -20,6 +22,10 @@ public class MainActivity extends Activity implements OnClickListener{
 	private static final String LOG_TAG = "MainActivity";	
 	private Switch playRecordSwitch;
 	public static String mediaFileNamePart;
+	
+	private SoundPool soundPool;
+	private int[] soundIDs = new int[]{-1,-1,-1,-1};
+	private int[] oldStreamIDs = new int[4];
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,27 +45,55 @@ public class MainActivity extends Activity implements OnClickListener{
 			}
 		});
 		
+		soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+		soundIDs[0] = soundPool.load(this, R.raw.voice, 1);
+		soundIDs[1] = soundPool.load(this, R.raw.drum, 1);
+		soundIDs[2] = soundPool.load(this, R.raw.hihat, 1);
+		soundIDs[3] = soundPool.load(this, R.raw.scratch, 1);
+		
 		mediaFileNamePart = Environment.getExternalStorageDirectory().getAbsolutePath();
 		mediaFileNamePart += "/SoundBoard_";
 	}
 
 	@Override
 	public void onClick(View v) {
+		
+		AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+		float actualVolume = (float)audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+		float maxVolume = (float)audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+		float volume = actualVolume / maxVolume;
+		
 		switch(v.getId()) {
 			case R.id.button1:
 				Log.i(LOG_TAG, "button1.onClick()");
+				if (oldStreamIDs[0] != 0) {
+					soundPool.stop(oldStreamIDs[0]);
+				}
+				oldStreamIDs[0] = soundPool.play(soundIDs[0], volume, volume, 1, 0, 1f);
 				StateMachine.getInstance().handleEvent(new Event(Event.Types.CLICK_EVENT, v.getId()));
 				break;
 			case R.id.button2:
 				Log.i(LOG_TAG, "button2.onClick()");
+				if (oldStreamIDs[1] != 0) {
+					soundPool.stop(oldStreamIDs[1]);
+				}
+				oldStreamIDs[1] = soundPool.play(soundIDs[1], volume, volume, 1, 0, 1f);
 				StateMachine.getInstance().handleEvent(new Event(Event.Types.CLICK_EVENT, v.getId()));
 			 	break;
 			case R.id.button3:
 				Log.i(LOG_TAG, "button3.onClick()");
+				if (oldStreamIDs[2] != 0) {
+					soundPool.stop(oldStreamIDs[2]);
+				}
+				oldStreamIDs[2] = soundPool.play(soundIDs[2], volume, volume, 1, 0, 1f);
 				StateMachine.getInstance().handleEvent(new Event(Event.Types.CLICK_EVENT, v.getId()));
 				break;
 			case R.id.button4:
 				Log.i(LOG_TAG, "button4.onClick()");
+				if (oldStreamIDs[3] != 0) {
+					soundPool.stop(oldStreamIDs[3]);
+				}
+				oldStreamIDs[3] = soundPool.play(soundIDs[3], volume, volume, 1, 0, 1f);
 				StateMachine.getInstance().handleEvent(new Event(Event.Types.CLICK_EVENT, v.getId()));
 				break;
 			default:
